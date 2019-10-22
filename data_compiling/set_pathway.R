688f2268fb98a6242672f9c085f16e4072e22039
Lauren Hallett <hallett@uoregon.edu>
2019-10-22 15:40:33 +0200

    update datpath for everyone

--- data_compiling/set_pathway.R
+++ data_compiling/set_pathway.R
@@ -5,7 +5,31 @@
 # If you have a Mac, run this line:
 datpath <- "~/Dropbox/sToration/YGW_data/"
 
+#LGS pathway 
+# datpath <- "~/Dropbox/Shared/sToration/YGW_data/"
+
 # If you have a PC, create your own version below and run it
 
 datpath <- "C:/Users/Lina/sToration/YGW_data/"
+
+# AMF pathway
+# datpath <- "C:/Users/akash/Dropbox/sToration/YGW_data/"
+
+##LL pathway
+#datpath <- "C:/Users/llari/Dropbox/sToration/YGW_data/"
+
+# LA pathway
+datpath <- "C:/Users/Lina/Dropbox/sToration/YGW_data/"
+
+# NAS pathway
+datpath <- "C:\\Users\\Nancy\\Dropbox\\sToration (1)\\YGW_data\\"
+
+# VT pathway 
+datpath <- "C:/Users/Vicky Temperton/Dropbox/sToration/YGW_data/"
+
+# Stan
+datpath <- "~/Dropbox (iDiv)/sToration/YGW_data/"
+
+#Ben
+datpath<-"C:/Users/Benjamin/Dropbox (Personal)/Research/SDiv coexistence restoration/sToration/YGW_data/"
 
